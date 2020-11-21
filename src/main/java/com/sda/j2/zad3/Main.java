9dde6b48f047b1767cd8262e1b0d3c1bb80366f6
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2020-11-21 10:56:58 +0100

    - j2: zad3 done

---
+++ src/main/java/com/sda/j2/zad3/Main.java
@@ -0,0 +1,46 @@
+package com.sda.j2.zad3;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Java", 18);
+        map.put("C#", 9);
+        map.put("Python", 1);
+        map.put("JavaScript", 0);
+        map.put("PHP", 0);
+
+        print(map);
+
+        System.out.println();
+        System.out.println("====================");
+        System.out.println();
+
+        printWithStream(map);
+    }
+
+    private static void print(Map<String, Integer> map) {
+
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            /*if(++i == map.size()) {
+                System.out.println("Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue() + ".");
+            } else {
+                System.out.println("Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue() + ",");
+            }*/
+
+            System.out.println("Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue() +
+                    (++i == map.size() ? "." : ","));
+        }
+    }
+
+    private static void printWithStream(Map<String, Integer> map) {
+        System.out.println(map.entrySet().stream()
+                .map(entry -> "Klucz: " + entry.getKey() + ", Wartość: " + entry.getValue())
+                .collect(Collectors.joining("," + System.lineSeparator(), "", ".")));
+    }
+}
---
+++ src/main/java/com/sda/j2/zad3/readme.md
@@ -0,0 +1,9 @@
+Zadanie 3.
+Stwórz metodę, która jako parametr przyjmuje mapę, gdzie kluczem jest string, a wartością liczba, a
+następnie wypisuje każdy element mapy do konsoli w formacie: Klucz: <k>, Wartość: <v>. Na końcu
+każdego wiersza poza ostatnim, powinien być przecinek, a w ostatnim kropka.
+Przykład:
+Klucz: Java, Wartość: 18,
+Klucz: Python, Wartość: 1,
+…
+Klucz: PHP, Wartość: 0.
