6cb7932ad138b0e578bbf972563e7568a477164e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2020-11-21 11:01:34 +0100

    - j2: zad4 draft

---
+++ src/main/java/com/sda/j2/examples/zad4/Storage.java
@@ -0,0 +1,27 @@
+package com.sda.j2.examples.zad4;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Storage {
+
+    private final Map<String, List<String>> storage;
+
+    public Storage() {
+        storage = new HashMap<>();
+    }
+
+    public void addToStorage(String key, String value) {
+
+    }
+
+    public void printValues(String key) {
+
+    }
+
+    public void findValues(String value) {
+
+    }
+
+}
---
+++ src/main/java/com/sda/j2/examples/zad4/readme.md
@@ -0,0 +1,7 @@
+Zadanie 4.
+Stwórz klasę Storage, która będzie miała prywatne pole typu Map, publiczny konstruktor oraz metody:
+addToStorage(String key, String value) → dodawanie elementów do przechowywalni
+printValues(String key) → wyświetlanie wszystkich elementów pod danym kluczem
+findValues(String value) → wyświetlanie wszystkich kluczy, które mają podaną wartość
+
+Klasa Storage powinna pozwalać na przechowywanie wielu wartości pod jednym kluczem.
