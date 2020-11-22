27bdfb5c3ef2d1edc7f6186e04c1ab5f4f32ba0a
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2020-11-22 09:52:25 +0100

    - j2: zad13 done

---
+++ src/main/java/com/sda/j2/examples/zad13/Manufacturer.java
@@ -0,0 +1,44 @@
+package com.sda.j2.examples.zad13;
+
+import java.util.Objects;
+
+public class Manufacturer {
+    private final String name;
+    private final int foundedInYear;
+    private final String countryOfOrigin;
+
+    public Manufacturer(String name, int foundedInYear, String countryOfOrigin) {
+        this.name = name;
+        this.foundedInYear = foundedInYear;
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFoundedInYear() {
+        return foundedInYear;
+    }
+
+    public String getCountryOfOrigin() {
+        return countryOfOrigin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manufacturer that = (Manufacturer) o;
+        return foundedInYear == that.foundedInYear &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(countryOfOrigin, that.countryOfOrigin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, foundedInYear, countryOfOrigin);
+    }
+
+
+}
---
+++ src/main/java/com/sda/j2/examples/zad13/readme.md
@@ -0,0 +1,13 @@
+Stwórz klasę CarService , która będzie zawierać w sobie listę aut, oraz będzie realizować poniższe metody:
+1. dodawanie aut do listy,
+2. usuwanie auta z listy,
+3. zwracanie listy wszystkich aut,
+4. zwracanie aut z silnikiem V12,
+5. zwracanie aut wyprodukowanych przed rokiem 1999,
+6. zwracanie najdroższego auta,
+7. zwracanie najtańszego auta,
+8. zwracanie auta z co najmniej 3 producentami,
+9. zwracanie listy wszystkich aut posortowanych zgodnie z przekazanym parametrem: rosnąco/malejąco,
+10. sprawdzanie czy konkretne auto znajduje się na liście,
+11. zwracanie listy aut wyprodukowanych przez konkretnego producenta,
+12. zwracanie listy aut wyprodukowanych przez producenta z rokiem założenia <,>,<=,>=,=,!= od podanego.
