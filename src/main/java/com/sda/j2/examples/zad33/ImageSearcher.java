d0a465f965de1197ce1567c0b76d2513ee7da1bd
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2020-11-28 13:08:48 +0100

    - j2: zad33: other ways to traverse

--- src/main/java/com/sda/j2/examples/zad33/ImageSearcher.java
+++ src/main/java/com/sda/j2/examples/zad33/ImageSearcher.java
@@ -1,38 +1,60 @@
 package com.sda.j2.examples.zad33;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ImageSearcher {
 
     public List<File> getImages(Path inputDirectory) {
-        /*dir.toFile().isDirectory();
-        File[] files = dir.toFile().listFiles((dir1, name) -> {
-            String toLowerCase = name.toLowerCase();
-            return toLowerCase.endsWith(".png")
-                    || toLowerCase.endsWith(".jpg");
-        });
-        for (File file : files) {
-            if(file.isDirectory()) {
-                //reccurence call?
-            } else {
-                file.getName()
+        //NOT GOOD, we're missing files from subdirectories !
+        File inputFile = inputDirectory.toFile();
+        if (inputFile.isDirectory()) {
+            File[] filtered = inputFile.listFiles((dir, name) -> {
+                String lowerCase = name.toLowerCase();
+                return lowerCase.endsWith(".png") || lowerCase.endsWith("jpg");
+            });
+
+            if (filtered != null) {
+                return Arrays.asList(filtered);
             }
-        }*/
-
-        return null;
+        }
+        return Collections.emptyList();
 
     }
 
-    public List<File> getImagesWithWalk(Path inputDirectory) {
-        //Files.walk()
-        return null;
+    public List<String> getImagesWithWalk(Path inputDirectory) throws IOException {
+        try (Stream<Path> walk = Files.walk(inputDirectory, 1)) {
+            return walk
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .filter(s -> s.endsWith(".png") || s.endsWith(".jpg"))
+                    .collect(Collectors.toList());
+        }
     }
 
-    public List<File> getImagesWithWalkTree(Path inputDirectory) {
-        //Files.walkFileTree()
-        return null;
+    public List<File> getImagesWithWalkTree(Path inputDirectory) throws IOException {
+        List<File> files = new ArrayList<>();
+        Files.walkFileTree(inputDirectory, Collections.emptySet(), 1, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                File f = file.toFile();
+                if (f.getName().endsWith(".jpg") || f.getName().endsWith(".png")) {
+                    files.add(f);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return files;
     }
 }
--- src/main/java/com/sda/j2/examples/zad33/Main.java
+++ src/main/java/com/sda/j2/examples/zad33/Main.java
@@ -3,11 +3,13 @@ package com.sda.j2.examples.zad33;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
-        Path dir;
+        ImageSearcher imageSearcher = new ImageSearcher();
+        System.out.println(imageSearcher.getImages(Paths.get("src", "main", "resources", "zad33")));
 
     }
 }
