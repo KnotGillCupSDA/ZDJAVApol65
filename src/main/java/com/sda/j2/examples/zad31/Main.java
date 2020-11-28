aea75b470df122c6d319e338e6aabe4c4f593dd5
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2020-11-28 11:06:39 +0100

    - j2: zad31 draft

--- src/main/java/com/sda/j2/examples/zad31/Main.java
+++ src/main/java/com/sda/j2/examples/zad31/Main.java
@@ -1,4 +1,35 @@
 package com.sda.j2.examples.zad31;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> counter;
+
+
+        Path path = ...;
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+        for (String line : lines) {
+            String normalizedLine = line.replaceAll("[^\\p{L}\\s]", "");
+
+            String[] words = normalizedLine.split(" ");
+
+            for (String word : words) {
+                //process word
+            }
+        }
+
+        //we should have counter map filled
+
+        System.out.println(counter);
+
+        //write to file
+
+    }
 }
